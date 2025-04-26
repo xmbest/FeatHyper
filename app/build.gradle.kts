9196e36b9909d8a54e8d28baa8e59dc4db7bd4e4
xmbest <lxmbest@foxmail.com>
2025-04-26 15:08:52 +0800

    feat: 更新版本号1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "me.xmbest.hyper"
         minSdk = 33
         targetSdk = 33
-        versionCode = 100_000_002
-        versionName = "1.0.2"
+        versionCode = 100_000_003
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
